8a8c126dada14a87d7206b66ba421d69c601b650
Raghean <83543261+Raghean@users.noreply.github.com>
2022-11-25 08:53:45 -0500

    Add files via upload

---
+++ zoo1.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#create a function called hours()
+def hours(str):
+    "The business hours open"
+    print(str)
+    return;
+
+#now call function
+hours("Open 9 to 5 daily!")
+
+
+# In[ ]:
+
+
+
+
