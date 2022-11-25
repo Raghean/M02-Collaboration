e9e9e6410572289597a84bbf23f582e98b5b1e75
Raghean <83543261+Raghean@users.noreply.github.com>
2022-11-25 08:44:03 -0500

    Add files via upload

---
+++ zoo.py
@@ -0,0 +1,84 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 16,
+   "id": "7fd0f284",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Open 9 to 5 daily!\n"
+     ]
+    }
+   ],
+   "source": [
+    "#create a function called hours()\n",
+    "def hours(str):\n",
+    "    \"The business hours open\"\n",
+    "    print(str)\n",
+    "    return;\n",
+    "\n",
+    "#now call function\n",
+    "hours(\"Open 9 to 5 daily!\")\n",
+    "    "
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 17,
+   "id": "353421a7",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Open 9 to 5 daily!\n"
+     ]
+    }
+   ],
+   "source": [
+    "#create a function called hours()\n",
+    "def hours(str):\n",
+    "    \"The business hours open\"\n",
+    "    print(str)\n",
+    "    return;\n",
+    "\n",
+    "#now call function\n",
+    "hours(\"Open 9 to 5 daily!\")"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "b06fd919",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.9.12"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
